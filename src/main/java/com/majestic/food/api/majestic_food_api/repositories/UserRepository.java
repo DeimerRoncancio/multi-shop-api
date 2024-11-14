af81df0147b2ec0d9322dabd18697b4f54485ff5
DeimerRoncancio <deimerroncancio@gmail.com>
2024-11-14 14:34:08 -0500

    "UserDetails" para autenticación implementado

--- src/main/java/com/majestic/food/api/majestic_food_api/repositories/UserRepository.java
+++ src/main/java/com/majestic/food/api/majestic_food_api/repositories/UserRepository.java
@@ -1,7 +1,11 @@
 package com.majestic.food.api.majestic_food_api.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.majestic.food.api.majestic_food_api.entities.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByName(String name);
 }
---
+++ src/main/java/com/majestic/food/api/majestic_food_api/services/UserDetailsServiceImpl.java
@@ -0,0 +1,50 @@
+package com.majestic.food.api.majestic_food_api.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.majestic.food.api.majestic_food_api.entities.User;
+import com.majestic.food.api.majestic_food_api.repositories.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+    
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<User> optionalUser = repository.findByName(username);
+
+        if (optionalUser.isEmpty())
+            throw new UsernameNotFoundException(String.format("El usuario %s no existe", username));
+
+        User user = optionalUser.orElseThrow();
+
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+            .map(role -> new SimpleGrantedAuthority(role.getRole()))
+            .collect(Collectors.toList());
+        
+        return new org.springframework.security.core.userdetails.User(
+            user.getName(), user.getPassword(),
+            true,
+            true,
+            true,
+            true,
+            authorities
+        );
+    }
+}
