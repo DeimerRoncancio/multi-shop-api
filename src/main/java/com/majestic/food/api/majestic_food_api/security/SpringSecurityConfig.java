b91f89096f8137332227950005b5b23675073c97
DeimerRoncancio <deimerroncancio@gmail.com>
2024-11-15 11:38:28 -0500

    Filtros y configuración de cors agregados a configuración de Spring Security

--- src/main/java/com/majestic/food/api/majestic_food_api/security/SpringSecurityConfig.java
+++ src/main/java/com/majestic/food/api/majestic_food_api/security/SpringSecurityConfig.java
@@ -2,15 +2,37 @@ package com.majestic.food.api.majestic_food_api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import com.majestic.food.api.majestic_food_api.security.filters.JwtAuthenticationFilter;
+import com.majestic.food.api.majestic_food_api.security.filters.JwtValidationFilter;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 
 @Configuration
 public class SpringSecurityConfig {
+
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
+    AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -25,13 +47,35 @@ public class SpringSecurityConfig {
             .requestMatchers(HttpMethod.GET, "/app/users/{id}").permitAll()
             .requestMatchers(HttpMethod.PUT, "/app/users/{id}").permitAll()
             .requestMatchers(HttpMethod.DELETE, "/app/users/{id}").permitAll()
-            .requestMatchers(HttpMethod.GET, "/app/categories").permitAll()
-            .requestMatchers(HttpMethod.POST, "/app/categories").permitAll()
-            .requestMatchers(HttpMethod.PUT, "/app/categories/{id}").permitAll()
-            .requestMatchers(HttpMethod.DELETE, "/app/categories/{id}").permitAll()
             .anyRequest().authenticated())
+            .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+            .addFilter(new JwtValidationFilter(authenticationManager()))
             .csrf(config -> config.disable())
             .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .build();
+    }
+
+    @Bean
+    CorsConfigurationSource sourceConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOriginPatterns(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "content-type"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        
+        return source;
+    }
+
+    @Bean
+    FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> corsBean = new FilterRegistrationBean<> (new CorsFilter(sourceConfigurationSource()));
+
+        corsBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return corsBean;
     }
 }
