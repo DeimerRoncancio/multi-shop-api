58b6e51473845f2007afa674b7b44a4c26beb060
DeimerRoncancio <deimerroncancio@gmail.com>
2024-11-22 20:08:11 -0500

    Asignación de roles a controlador "RoleController"

--- src/main/java/com/majestic/food/api/majestic_food_api/controllers/RoleController.java
+++ src/main/java/com/majestic/food/api/majestic_food_api/controllers/RoleController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,11 +22,13 @@ public class RoleController {
     private RoleService service;
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public List<Role> viewAll() {
         return service.findAll();
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Role> view(@PathVariable String id) {
         Optional<Role> roleDb = service.findOne(id);
 
